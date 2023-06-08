036520209638ad90655532460d7f3c20a6c0c025
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2023-06-08 16:15:51 -0500

    starting stuff

---
+++ pal.py
@@ -0,0 +1,12 @@
+import pal
+from pal.prompt import math_prompts
+
+interface = pal.interface.ProgramInterface(
+  model='code-davinci-002',
+  stop='\n\n\n', # stop generation str for Codex API
+  get_answer_expr='solution()' # python expression evaluated after generated code to obtain answer 
+)
+
+question = 'The bakers at the Beverly Hills Bakery baked 200 loaves of bread on Monday morning. They sold 93 loaves in the morning and 39 loaves in the afternoon. A grocery store returned 6 unsold loaves. How many loaves of bread did they have?'
+prompt = math_prompts.MATH_PROMPT.format(question=question)
+answer = interface.run(prompt)
