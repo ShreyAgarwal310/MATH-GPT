fdb99a09745370d104f7f97d6bf411726902eebe
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2023-06-15 10:27:50 -0500

    Update symbolic_solver.py

--- symbolic_solver.py
+++ symbolic_solver.py
@@ -133,4 +133,3 @@ def get_final_using_sympy(equations):
 question = 'Bob says to Alice: if you give me 3 apples and then take half of my apples away, then I will be left with 13 apples. How many apples do I have now?'
 eq_list = get_declarative_equations(model='text-davinci-003', question=question, prompt=DECLARATIVE_THREE_SHOT_AND_PRINCIPLES, max_tokens=600, stop_token='\n\n\n', temperature=2)
 answer = get_final_using_sympy(eq_list)
-print(answer)
