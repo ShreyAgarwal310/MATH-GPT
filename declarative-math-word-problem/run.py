b09dcee508619776619aa91ccac310dadbf47116
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2023-06-17 10:37:07 -0500

    Update run.py

--- declarative-math-word-problem/run.py
+++ declarative-math-word-problem/run.py
@@ -1,7 +1,20 @@
 from utils import *
 from prompts.declarative_three_shot import DECLARATIVE_THREE_SHOT_AND_PRINCIPLES
+import openai
+
+def get_file_contents(filename):
+    try:
+        with open(filename, 'r') as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        print("'%s' file not found" % filename)
+
+api_key = get_file_contents('api_key.txt')
+openai.api_key = api_key
 
 question = 'Bob says to Alice: if you give me 3 apples and then take half of my apples away, then I will be left with 13 apples. How many apples do I have now?'
+question = input("Enter a mathematical expression: ")
+
 eq_list = get_declarative_equations(model='text-davinci-003', question=question, prompt=DECLARATIVE_THREE_SHOT_AND_PRINCIPLES, max_tokens=600, stop_token='\n\n\n', temperature=0)
 answer = get_final_using_sympy(eq_list)
 print(answer)
