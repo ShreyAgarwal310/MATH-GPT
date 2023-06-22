67f25ce24525cc41f364b8f94110cb79654b9114
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2023-06-22 14:29:05 -0500

    adding langchain solver

--- langchain_test.py
+++ langchain_test.py
@@ -18,55 +18,122 @@ os.environ["OPENAI_API_KEY"] = api_key
 
 llm = OpenAI(model_name='text-davinci-003', temperature=0)
 
-question = "Today is 27 February 2023. I was born exactly 25 years ago. What is the date I was born in MM/DD/YYYY?"
+question = input("Enter a mathematical expression: ")
  
-DATE_UNDERSTANDING_PROMPT = """
-# Q: 2015 is coming in 36 hours. What is the date one week from today in MM/DD/YYYY?
-# If 2015 is coming in 36 hours, then today is 36 hours before.
-today = datetime(2015, 1, 1) - relativedelta(hours=36)
-# One week from today,
-one_week_from_today = today + relativedelta(weeks=1)
-# The answer formatted with %m/%d/%Y is
-one_week_from_today.strftime('%m/%d/%Y')
-# Q: The first day of 2019 is a Tuesday, and today is the first Monday of 2019. What is the date today in MM/DD/YYYY?
-# If the first day of 2019 is a Tuesday, and today is the first Monday of 2019, then today is 6 days later.
-today = datetime(2019, 1, 1) + relativedelta(days=6)
-# The answer formatted with %m/%d/%Y is
-today.strftime('%m/%d/%Y')
-# Q: The concert was scheduled to be on 06/01/1943, but was delayed by one day to today. What is the date 10 days ago in MM/DD/YYYY?
-# If the concert was scheduled to be on 06/01/1943, but was delayed by one day to today, then today is one day later.
-today = datetime(1943, 6, 1) + relativedelta(days=1)
-# 10 days ago,
-ten_days_ago = today - relativedelta(days=10)
-# The answer formatted with %m/%d/%Y is
-ten_days_ago.strftime('%m/%d/%Y')
-# Q: It is 4/19/1969 today. What is the date 24 hours later in MM/DD/YYYY?
-# It is 4/19/1969 today.
-today = datetime(1969, 4, 19)
-# 24 hours later,
-later = today + relativedelta(hours=24)
-# The answer formatted with %m/%d/%Y is
-today.strftime('%m/%d/%Y')
-# Q: Jane thought today is 3/11/2002, but today is in fact Mar 12, which is 1 day later. What is the date 24 hours later in MM/DD/YYYY?
-# If Jane thought today is 3/11/2002, but today is in fact Mar 12, then today is 3/1/2002.
-today = datetime(2002, 3, 12)
-# 24 hours later,
-later = today + relativedelta(hours=24)
-# The answer formatted with %m/%d/%Y is
-later.strftime('%m/%d/%Y')
-# Q: Jane was born on the last day of February in 2001. Today is her 16-year-old birthday. What is the date yesterday in MM/DD/YYYY?
-# If Jane was born on the last day of February in 2001 and today is her 16-year-old birthday, then today is 16 years later.
-today = datetime(2001, 2, 28) + relativedelta(years=16)
-# Yesterday,
-yesterday = today - relativedelta(days=1)
-# The answer formatted with %m/%d/%Y is
-yesterday.strftime('%m/%d/%Y')
-# Q: {question}
-""".strip() + '\n'
+MATH_PROMPT = '''
+Q: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
 
-llm_out = llm(DATE_UNDERSTANDING_PROMPT.format(question=question))
+# solution in Python:
+
+def solution():
+    """Olivia has $23. She bought five bagels for $3 each. How much money does she have left?"""
+    money_initial = 23
+    bagels = 5
+    bagel_cost = 3
+    money_spent = bagels * bagel_cost
+    money_left = money_initial - money_spent
+    result = money_left
+    return result
+
+Q: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
+
+# solution in Python:
+
+def solution():
+    ""Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?""
+    golf_balls_initial = 58
+    golf_balls_lost_tuesday = 23
+    golf_balls_lost_wednesday = 2
+    golf_balls_left = golf_balls_initial - golf_balls_lost_tuesday - golf_balls_lost_wednesday
+    result = golf_balls_left
+    return result
+
+Q: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
+
+# solution in Python:
+
+def solution():
+    ""There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?""
+    computers_initial = 9
+    computers_per_day = 5
+    num_days = 4  # 4 days between monday and thursday
+    computers_added = computers_per_day * num_days
+    computers_total = computers_initial + computers_added
+    result = computers_total
+    return result
+
+Q: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
+
+# solution in Python:
+
+def solution():
+    ""Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?""
+    toys_initial = 5
+    mom_toys = 2
+    dad_toys = 2
+    total_received = mom_toys + dad_toys
+    total_toys = toys_initial + total_received
+    result = total_toys
+    return result
+
+Q: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
+
+# solution in Python:
+
+def solution():
+    ""Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?""
+    jason_lollipops_initial = 20
+    jason_lollipops_after = 12
+    denny_lollipops = jason_lollipops_initial - jason_lollipops_after
+    result = denny_lollipops
+    return result
+
+Q: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
+
+# solution in Python:
+
+def solution():
+    ""Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?""
+    leah_chocolates = 32
+    sister_chocolates = 42
+    total_chocolates = leah_chocolates + sister_chocolates
+    chocolates_eaten = 35
+    chocolates_left = total_chocolates - chocolates_eaten
+    result = chocolates_left
+    return result
+
+Q: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
+
+# solution in Python:
+
+def solution():
+    ""If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?""
+    cars_initial = 3
+    cars_arrived = 2
+    total_cars = cars_initial + cars_arrived
+    result = total_cars
+    return result
+
+Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?
+
+# solution in Python:
+
+
+def solution():
+    ""There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?"""
+    trees_initial = 15
+    trees_after = 21
+    trees_added = trees_after - trees_initial
+    result = trees_added
+    return result
+
+Q: {question}
+
+# solution in Python:
+'''.strip() + '\n\n\n'
+
+llm_out = llm(MATH_PROMPT.format(question=question))
 print(llm_out)
 
 exec(llm_out)
-# says it'll throw an error but it won't
-print(born)
+#print(result)
